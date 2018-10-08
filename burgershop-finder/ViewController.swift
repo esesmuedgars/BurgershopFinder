97efce93fed8abdf5b6033bd22456352c5b01f9e
Edgars Vanags <edgars.vanags1@gmail.com>
2018-10-08 12:42:53 +0300

    Initial commit

---
+++ burgershop-finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  burgershop-finder
+//
+//  Created by e.vanags on 08/10/2018.
+//  Copyright © 2018 esesmuedgars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
