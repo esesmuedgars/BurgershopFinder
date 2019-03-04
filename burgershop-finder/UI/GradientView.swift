736c007be36c7d1adaf01286d39700c7e3b6180f
Edgars Vanags <e.vanags@accenture.com>
2019-03-05 00:28:05 +0200

    Quality of service: Adjusted HomeViewController scrollView. Added colorBlending mode to GradientView

--- burgershop-finder/UI/GradientView.swift
+++ burgershop-finder/UI/GradientView.swift
@@ -7,27 +7,62 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 @IBDesignable
 final class GradientView: SetupView {
+
+    private lazy var disposeBag = DisposeBag()
+
+    private let setTopColor = BehaviorSubject<UIColor>(value: .white)
+    private let setBottomColor = BehaviorSubject<UIColor>(value: .white)
+    private let setColorBlending = PublishSubject<Void>()
+
     override func setup() {
         autoresizingMask = [.flexibleWidth, .flexibleHeight]
         layer.frame = bounds
+
+        bindRx()
+    }
+
+    private func bindRx() {
+        let colors = Observable.zip(setTopColor.asObservable(), setBottomColor.asObservable())
+
+        setColorBlending
+            .startWith(())
+            .withLatestFrom(colors)
+            .subscribe(onNext: { [unowned self] top, bottom in
+                guard let layer = self.layer as? CAGradientLayer else { return }
+
+                if self.colorBlending {
+                    layer.colors = [top.cgColor, bottom.cgColor]
+                } else {
+                    layer.colors = [top.cgColor, top.cgColor, bottom.cgColor, bottom.cgColor]
+                    layer.locations = [0, 0.5, 0.5, 1]
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
     @IBInspectable
     public var topColor: UIColor = .white {
         didSet {
-            guard let layer = self.layer as? CAGradientLayer else { return }
-            layer.colors = [topColor.cgColor, bottomColor.cgColor]
+            setTopColor.onNext(topColor)
         }
     }
 
     @IBInspectable
     public var bottomColor: UIColor = .white {
         didSet {
-            guard let layer = self.layer as? CAGradientLayer else { return }
-            layer.colors = [topColor.cgColor, bottomColor.cgColor]
+            setBottomColor.onNext(bottomColor)
+        }
+    }
+
+    @IBInspectable
+    public var colorBlending: Bool = true {
+        didSet {
+            setColorBlending.onNext(())
         }
     }
 
@@ -35,4 +70,3 @@ final class GradientView: SetupView {
         return CAGradientLayer.self
     }
 }
-
